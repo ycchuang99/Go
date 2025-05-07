87d54397f069e48a8c723af57c136914834320dd
Evan C <ycchuang1999@gmail.com>
2025-05-07 16:38:50 +0800

    run go lint

--- 00-basics/05-for/for.go
+++ 00-basics/05-for/for.go
@@ -4,29 +4,29 @@ import "fmt"
 
 func main() {
 
-    i := 1
-    for i <= 3 {
-        fmt.Println(i)
-        i = i + 1
-    }
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
-    for j := 0; j < 3; j++ {
-        fmt.Println(j)
-    }
+	for j := 0; j < 3; j++ {
+		fmt.Println(j)
+	}
 
-    for i := range 3 {
-        fmt.Println("range", i)
-    }
+	for i := range 3 {
+		fmt.Println("range", i)
+	}
 
-    for {
-        fmt.Println("loop")
-        break
-    }
+	for {
+		fmt.Println("loop")
+		break
+	}
 
-    for n := range 6 {
-        if n%2 == 0 {
-            continue
-        }
-        fmt.Println(n)
-    }
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
